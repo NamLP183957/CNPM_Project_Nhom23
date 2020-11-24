494a6f455599f6e532c1c82a106763d4c268e1e0
nghialeminhbk <nghia9c.12@gmail.com>
2020-11-25 00:03:07 +0700

    changed file with delete a line

--- src/Model/PhanHoiModify.java
+++ src/Model/PhanHoiModify.java
@@ -9,9 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
-import java.util.Vector;
-import javax.swing.JOptionPane;
-
 import Controller.SQLconnection;
 import java.sql.Date;
 
