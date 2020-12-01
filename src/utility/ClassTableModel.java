efd4ec26353e26285222e93508fe5eccc2dbcbd1
Namlsm <namcongngheit3@gmail.com>
2020-12-01 08:13:14 +0700

    create ClassTableModel

---
+++ src/utility/ClassTableModel.java
@@ -0,0 +1,51 @@
+
+package utility;
+
+import java.util.List;
+import javafx.util.Pair;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import models.PhanAnh;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ClassTableModel {
+   
+    public DefaultTableModel setPhanAnhTable(List<PhanAnh> listPhanAnh, JTable phanAnhTable) {
+        
+        DefaultTableModel dtm = (DefaultTableModel) phanAnhTable.getModel();
+        dtm.setRowCount(0);
+        dtm.setColumnIdentifiers(new Object[] {
+            "STT", "Người phản ánh", "Ngày", "Nội dung", "Lĩnh vực", "Trạng thái"
+        });
+        
+        for(int i = 0; i < listPhanAnh.size(); i++) {
+            PhanAnh phanAnh = listPhanAnh.get(i);
+            dtm.addRow(new Object[]{
+                i+1, phanAnh.getName(), phanAnh.getDate(), phanAnh.getContent(), phanAnh.getType(), phanAnh.getState()
+            });
+        }
+        
+        return dtm;
+    }
+    
+    public DefaultTableModel setThongKeTable(String tieuChi, List<Pair<Integer, String> > list, JTable jtb){
+        DefaultTableModel dtm = (DefaultTableModel) jtb.getModel();
+        dtm.setRowCount(0);
+        dtm.setColumnIdentifiers(new Object[]{
+            tieuChi, "Số lượng"
+        });
+        
+        for(int i = 0; i < list.size(); i++){
+            String field = list.get(i).getValue();
+            int soLuong = list.get(i).getKey();
+            dtm.addRow(new Object[] {
+                field, soLuong
+            });
+        }
+        
+        return dtm;
+    }
+}
