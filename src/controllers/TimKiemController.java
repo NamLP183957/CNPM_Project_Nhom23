3a7aba586428d50712d6a6754a65ee6ef86ab017
tuyentran2222 <tranvantuyen02082000@gmail.com>
2020-12-01 08:59:49 +0700

    add TimKiemController

---
+++ src/controllers/TimKiemController.java
@@ -0,0 +1,120 @@
+package controllers;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import models.PhanAnh;
+import java.sql.*;
+import service.MySQLConnection;
+/**
+ *
+ * @author acer
+ */
+public class TimKiemController {
+        //tìm kiếm phản ánh theo ngày
+        public ArrayList<PhanAnh> getPhanAnhByDate(Date ngay) {
+            ArrayList<PhanAnh> list = new ArrayList<>();
+            Connection conn=MySQLConnection.getMySQLConnection();
+            String select = "select * from phan_anh where NGAY_PHAN_ANH='"+ngay+"'";
+            try (
+                PreparedStatement ps = conn.prepareStatement(select)) {
+
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    PhanAnh pa = new PhanAnh();                    
+                    pa.setId(rs.getInt("MA_PHAN_ANH"));
+                    pa.setName(rs.getString("NGUOI_PHAN_ANH"));
+                    pa.setContent(rs.getString("NOI_DUNG"));
+                    pa.setDate(rs.getDate("NGAY_PHAN_ANH"));
+                    pa.setType(rs.getString("PHAN_LOAI"));
+                    pa.setState(rs.getString("TRANG_THAI"));
+                    list.add(pa);
+                   System.out.println(pa.getId()+pa.getName()+pa.getState());
+
+                }
+            } catch (Exception e) {
+            }
+            return list;
+        }  
+        
+        public ArrayList<PhanAnh> getPhanAnhByState(String state) {
+            ArrayList<PhanAnh> list = new ArrayList<>();
+            Connection conn=MySQLConnection.getMySQLConnection();
+            String select = "select * from phan_anh where TRANG_THAI='"+state+"'";
+            //System.out.println(state);
+            try (
+                PreparedStatement ps = conn.prepareStatement(select)) {
+
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    PhanAnh pa = new PhanAnh();
+                    pa.setId(rs.getInt("MA_PHAN_ANH"));
+                    pa.setName(rs.getString("NGUOI_PHAN_ANH"));
+                    pa.setContent(rs.getString("NOI_DUNG"));
+                    pa.setDate(rs.getDate("NGAY_PHAN_ANH"));
+                    pa.setType(rs.getString("PHAN_LOAI"));
+                    pa.setState(rs.getString("TRANG_THAI"));
+                    list.add(pa);
+                    System.out.println(pa.getId()+pa.getName()+pa.getState());
+                }
+            } catch (Exception e) {
+            }
+            return list;
+        }
+        
+        //tìm kiếm tất cả phản ánh 
+        public ArrayList<PhanAnh> getAllPhanAnh() {
+            ArrayList<PhanAnh> list = new ArrayList<>();
+            Connection conn=MySQLConnection.getMySQLConnection();
+            String select = "select * from phan_anh";
+            try (
+                PreparedStatement ps = conn.prepareStatement(select)) {
+
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    PhanAnh pa = new PhanAnh();
+                    pa.setId(rs.getInt("MA_PHAN_ANH"));
+                    pa.setName(rs.getString("NGUOI_PHAN_ANH"));
+                    pa.setContent(rs.getString("NOI_DUNG"));
+                    pa.setDate(rs.getDate("NGAY_PHAN_ANH"));
+                    pa.setType(rs.getString("PHAN_LOAI"));
+                    pa.setState(rs.getString("TRANG_THAI"));
+                     list.add(pa);
+                    System.out.println(pa.getId()+pa.getName()+pa.getContent()+pa.getDate()+pa.getType()+pa.getState());
+                }
+            } catch (Exception e) {
+            }
+            return list;
+        } 
+      
+      //tìm kiếm theo tên
+        public ArrayList<PhanAnh> getPhanAnhByName(String name) {
+            ArrayList<PhanAnh> list = new ArrayList<>();
+            Connection conn=MySQLConnection.getMySQLConnection();
+            String select = "select * from phan_anh where NGUOI_PHAN_ANH LIKE '%"+ name+ "%'";
+            try (
+                PreparedStatement ps = conn.prepareStatement(select)) {
+
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    PhanAnh pa = new PhanAnh();
+                    pa.setId(rs.getInt("MA_PHAN_ANH"));
+                    pa.setName(rs.getString("NGUOI_PHAN_ANH"));
+                    pa.setContent(rs.getString("NOI_DUNG"));
+                    pa.setDate(rs.getDate("NGAY_PHAN_ANH"));
+                    pa.setType(rs.getString("PHAN_LOAI"));
+                    pa.setState(rs.getString("TRANG_THAI"));
+                    list.add(pa);
+                    System.out.println(pa.getId()+pa.getName()+pa.getDate());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return list;
+          }
+     
+}
+
