2ae439d113cd1ccedeeed956e593949ad9a32540
tuyentran2222 <tranvantuyen02082000@gmail.com>
2020-12-03 23:46:05 +0700

    them model account

---
+++ src/models/Account.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author acer
+ */
+public class Account {
+    private int Id;
+    private String password;
+    private String userName;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    
+
+   
+
+    
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+    
+}
